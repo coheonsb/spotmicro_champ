65a45040c2b3b54d2f81b2faf823a6d6b0c17fb7
coheonsb <coheonsb@gmail.com>
2023-04-29 02:09:13 +0900

    Update servo_interface.py

--- src/robots/configs/spotmicro_config/src/servo_interface.py
+++ src/robots/configs/spotmicro_config/src/servo_interface.py
@@ -4,16 +4,49 @@ import rospy
 import numpy as np
 from trajectory_msgs.msg import JointTrajectory
 from sensor_msgs.msg import JointState
-from i2cpwm_board.msg import Servo, ServoArray
 from std_msgs.msg import Header
 import math
-# from MangDang.mini_pupper.HardwareInterface import HardwareInterface
 
-# hardware_interface = HardwareInterface()
+from i2cpwm_board.msg import Servo, ServoArray
+
+
+import busio
+import board
+import numpy as np
+from adafruit_servokit import ServoKit
+from adafruit_pca9685 import PCA9685
+
+
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+
+kit = ServoKit(channels=16)
+
+pca = PCA9685(i2c_bus)
+pca.frequency = 330
+
+
+config = [
+    {'id': 0, 'fix': 90, 'offSet': 0, 'direction': -1, 'name': 'front_left_shoulder', 'min_angle': 5, 'max_angle': 175},
+    {'id': 1, 'fix': 90, 'offSet': 0, 'direction': 1, 'name': 'front_left_leg', 'min_angle': 5, 'max_angle': 175},
+    {'id': 2, 'fix': 180, 'offSet': 10, 'direction': 1, 'name': 'front_left_foot', 'min_angle': 5, 'max_angle': 175},
+    {'id': 3, 'fix': 90, 'offSet': 0, 'direction': -1, 'name': 'front_right_shoulder', 'min_angle': 5, 'max_angle': 175},
+    {'id': 4, 'fix': 90, 'offSet': 0, 'direction': -1, 'name': 'front_right_leg', 'min_angle': 5, 'max_angle': 175},
+    {'id': 5, 'fix': 0, 'offSet': 0, 'direction': -1, 'name': 'front_right_foot', 'min_angle': 5, 'max_angle': 175},
+    {'id': 6, 'fix': 90, 'offSet': 0, 'direction': 1, 'name': 'rear_left_shoulder', 'min_angle': 5, 'max_angle': 175},
+    {'id': 7, 'fix': 90, 'offSet': 0, 'direction': 1, 'name': 'rear_left_leg', 'min_angle': 5, 'max_angle': 175},
+    {'id': 8, 'fix': 180, 'offSet': 0, 'direction': 1, 'name': 'rear_left_foot', 'min_angle': 5, 'max_angle': 175},
+    {'id': 9, 'fix': 90, 'offSet': 0, 'direction': 1, 'name': 'rear_right_shoulder', 'min_angle': 5, 'max_angle': 175},
+    {'id': 10, 'fix': 90, 'offSet': 0, 'direction': -1, 'name': 'rear_right_leg', 'min_angle': 5, 'max_angle': 175},
+    {'id': 11, 'fix': 0, 'offSet': 0, 'direction': -1, 'name': 'rear_right_foot', 'min_angle': 5, 'max_angle': 175}
+]
+
+
+for i in range(12):
+    kit.servo[i].set_pulse_width_range(500, 2500)
+
 
 joint_state_pub = rospy.Publisher('/joint_states', JointState, queue_size=1)
 joint_names = [
-   
     'front_left_shoulder',
     'front_left_leg',
     'front_left_foot',
@@ -29,92 +62,56 @@ joint_names = [
 ]
 
 
-
-
-def rTod(radians, config = 0.0):
+def rTod(radians):
     return math.degrees(radians)
 
-def dTor(degrees, config = 0.0):
+
+def dTor(degrees):
     return math.radians(degrees)
 
 
-
-def angle_to_pwm(angle_rad, min_pwm, max_pwm, center_pwm):
-    # Convert angle from radians to degrees
-    angle_deg = math.degrees(angle_rad)
-
-    # Map angle range (-90, 90) to PWM range (min_pwm, max_pwm)
-    pwm = int(((angle_deg + 90) * (max_pwm - min_pwm) / 180) + min_pwm)
-
-    # Make sure PWM value is within the allowed range
-    pwm = max(min(pwm, max_pwm), min_pwm)
-
-    return pwm
+def move_servo(servo_index, angle):
+    realId = config[servo_index].get('id')
+    fixValue = config[servo_index].get('fix')
+    direction = config[servo_index].get('direction')
+    offSet = config[servo_index].get('offSet')
+    finalAngle = fixValue + (angle * direction)
+    # print(finalAngle)
+    kit.servo[realId].angle = finalAngle
+    return angle + offSet
 
 
 def callback(msg):
     joint_positions = msg.points[0].positions
-    # rospy.loginfo(msg.points[0])
-    servo_array = ServoArray()
-    for i, position in enumerate(joint_positions):
-      servo = Servo()
-      servo.servo = i + 1
-      min_pwm = 83
-      max_pwm = 520
-      center_pwm = 306
-      pwm_value = angle_to_pwm(position, min_pwm, max_pwm, center_pwm)
-      position = 30
-    #   servo.value = pwm_value  # need chane angle to pwm
-    #   servo_array.servos.append(servo)
-
-    # servo_pub.publish(servo_array)
-    # publish_joint_states(joint_positions)
-    # rTod
-    # degs = tuple(rTod(jr) for jr in joint_positions)
-    degs = (rTod(joint_positions[0]), rTod(joint_positions[1]), rTod(joint_positions[2]),
-    rTod(joint_positions[3]), rTod(joint_positions[4]), rTod(joint_positions[5]),
-    rTod(joint_positions[6]), rTod(joint_positions[7]), rTod(joint_positions[8]),
-    rTod(joint_positions[9]), rTod(joint_positions[10]), rTod(joint_positions[11]))
+    degs = tuple(rTod(jr) for jr in joint_positions)
     rospy.loginfo(degs)
-    # radis = tuple(dTor(jd) for jd in degs)
-    radis = (dTor(degs[0]), dTor(degs[1]), dTor(degs[2]),
-    dTor(degs[3]), dTor(degs[4]), dTor(degs[5]),
-    dTor(degs[6]), dTor(degs[7]), dTor(degs[8]),
-    dTor(degs[9]), dTor(degs[10]), dTor(degs[11]))
+    for i, d in enumerate(degs):
+        move_servo(i, d)
+    radis = tuple(dTor(jd) for jd in degs)
     publish_joint_states(radis)
 
 
-config = [2.6181638120218977e-06, 64.89528704025946, -110.4209430661173,
-           -7.969308747622277e-07, 64.89528704025946, -110.4209430661173, 
-           2.6181638120218977e-06, 64.89528704025946, -110.4209430661173, 
-           -7.969308747622277e-07, 64.89528704025946, -110.4209430661173]
+# def listener():
+#     rospy.init_node('servo_interface', anonymous=True)
+#     rospy.Subscriber('/joint_group_position_controller/command',
+#                      JointTrajectory, callback, queue_size=1)
+#     rospy.spin()
 
-def listener():
-
-    rospy.init_node('servo_interface', anonymous=True)
-    rospy.Subscriber('/joint_group_position_controller/command',
-                     JointTrajectory, callback, queue_size=1)
-    rospy.spin()
 
 def publish_joint_states(joint_positions):
     joint_state = JointState()
-   
     joint_state.header = Header()
     joint_state.header.stamp = rospy.Time.now()
     joint_state.name = joint_names
     joint_state.position = joint_positions
 
-    # rospy.loginfo(joint_positions)
     joint_state_pub.publish(joint_state)
 
 
 if __name__ == '__main__':
     rospy.init_node("hardware_interface_node")
-
-    servo_pub = rospy.Publisher("/servos_absolute", ServoArray, queue_size=1)
-    joint_state_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
-
-    rospy.Subscriber("/joint_group_position_controller/command", JointTrajectory, callback, queue_size=1)
-
+    # servo_pub = rospy.Publisher("/servos_absolute", ServoArray, queue_size=1)
+    # joint_state_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
+    rospy.Subscriber("/joint_group_position_controller/command",
+                     JointTrajectory, callback, queue_size=1)
     rospy.spin()
-    # listener()
